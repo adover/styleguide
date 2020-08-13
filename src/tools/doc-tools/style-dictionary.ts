4c04c776b78e6266e5b1d9d2083c8f714077c845
Andy Dover <Andrew_dover@hotmail.com>
2020-08-13 20:27:14 +1200

    feat(styleguide): add style dict

--- src/tools/doc-tools/style-dictionary.ts
+++ src/tools/doc-tools/style-dictionary.ts
@@ -1,18 +1,4 @@
 // tslint:disable-next-line: typedef
-const StyleDictionary = require('style-dictionary').extend({
-	source: ['properties/**/*.json'],
-	platforms: {
-		scss: {
-			transformGroup: 'scss',
-			buildPath: 'build/',
-			files: [
-				{
-					destination: 'variables.scss',
-					format: 'scss/variables',
-				},
-			],
-		},
-	},
-});
+const StyleDictionary = require('style-dictionary').extend('./src/config.json');
 
 StyleDictionary.buildAllPlatforms();
